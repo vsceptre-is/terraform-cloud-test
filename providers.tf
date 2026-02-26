3d66a638c20f6330b8625378fbf04b5830074b09
Ivan Vsceptre <ivanseit@vsceptre.com>
2026-02-26 17:32:14 +0800

    add terraform

---
+++ providers.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  config_context = "my-context"
+host = var.host
+
+  client_certificate     = base64decode(var.client_certificate)
+  client_key             = base64decode(var.client_key)
+  cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"  # Or use cluster auth
+  }
+}
