4f913704541babf12fa00fcbc31e835007874bb5
Ivan Vsceptre <ivanseit@vsceptre.com>
2026-04-15 10:07:58 +0800

    change formatting

--- providers.tf
+++ providers.tf
@@ -36,26 +36,28 @@ resource "datadog_monitor" "slo_monitor_test" {
     critical = 90
   }
 }
+
 resource "datadog_monitor" "cpu_high" {
   name  = "tested_terraform_cloud3"
   type  = "metric alert"
   query = "avg(last_5m):avg:system.cpu.user{env:prod} by {host} > 80"
-
   message = "High CPU in prod."
-
   monitor_thresholds {
     critical = 80
     warning  = 70
   }
 }
+
 resource "datadog_team" "terraform_test"{
   description = "This is created using terraform"
   handle      = "terraform-team"
   name        = "terraform Team Test"
 }
+
 resource "datadog_user" "myself"{
   email = "ivanseit@vsceptre.com"
 }
+
 resource "datadog_team_membership" "foo" {
   team_id = datadog_team.terraform_test.id
   user_id = datadog_user.myself.id
